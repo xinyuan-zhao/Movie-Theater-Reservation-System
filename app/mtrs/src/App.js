8f748fde3f844077c93aa1931fa2c3c89537f80b
Shawn Fei <shawnfei2001@gmail.com>
2023-11-25 23:23:34 -0500

    user login, sign up and admin login

--- app/mtrs/src/App.js
+++ app/mtrs/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserLogin from './UserLogin';
+import UserSignUp from './UserSignUp';
+import AdminLogin from './AdminLogin';
 
-function App() {
+const App = () => {
+  const [active, setActive] = useState('userLogin');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Movie Theater Reservation System
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <button onClick={() => setActive('userLogin')}>User Login</button>
+        <button onClick={() => setActive('userSignUp')}>User Sign Up</button>
+        <button onClick={() => setActive('adminLogin')}>Admin Login</button>
       </header>
+      
+      {active === 'userLogin' && <UserLogin />}
+      {active === 'userSignUp' && <UserSignUp />}
+      {active === 'adminLogin' && <AdminLogin />}
     </div>
   );
-}
+};
 
 export default App;
