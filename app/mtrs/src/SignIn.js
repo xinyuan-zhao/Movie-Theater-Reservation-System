adbe2ceb8e0e22272760b89b402532208caf04e7
Shawn Fei <shawnfei2001@gmail.com>
2023-11-27 14:57:05 -0500

    Frontend

--- app/mtrs/src/SignIn.js
+++ app/mtrs/src/SignIn.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { Button, Grid, Box } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
-import Paper from '@mui/material/Paper';
+import { Button, Grid, Box, CssBaseline, Paper, Avatar, FormControlLabel, Checkbox } from '@mui/material';
 function Signin() {
   return (
     <Grid container component="main" sx={{ height: '100vh' }}>
@@ -33,6 +31,56 @@ function Signin() {
             alignItems: 'center',
           }}
         >
+          <Avatar sx={{ m: 1, bgcolor: 'green' }}></Avatar>
+          <Typography component="h1" variant="h5">
+            User Sign in
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
         </Box>
       </Grid>
     </Grid>
