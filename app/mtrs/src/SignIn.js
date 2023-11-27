dfde42bd135f645780574b83128c27d2e854b40b
Shawn Fei <shawnfei2001@gmail.com>
2023-11-27 17:44:10 -0500

    Login Finished

--- app/mtrs/src/SignIn.js
+++ app/mtrs/src/SignIn.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import Link from '@mui/material/Link';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import { Button, Grid, Box, CssBaseline, Paper, Avatar, FormControlLabel, Checkbox } from '@mui/material';
+import React, { useState } from 'react';
+import axios from 'axios';
+import SwitchRole from './components/SwitchRole';
+import { Link, TextField, Typography, Button, Grid, Box, CssBaseline, Paper, Avatar, FormControlLabel, Checkbox } from '@mui/material';
 function Signin() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      if (email.includes("@")){
+        await axios.post('http://localhost:5000/login', { email, password });
+        alert('Welcome User!');
+      } else {
+        await axios.post('http://localhost:5000/admin/login', { email, password });
+        alert('Welcome Admin!');
+      }
+    } catch (error) {
+      alert('Login failed!');
+    }
+  };
+
   return (
     <Grid container component="main" sx={{ height: '100vh' }}>
       <CssBaseline />
@@ -32,21 +49,26 @@ function Signin() {
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: 'green' }}></Avatar>
+          <SwitchRole/>
           <Typography component="h1" variant="h5">
-            User Sign in
+            Sign in
           </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
+          <Box component="form" noValidate onSubmit={handleLogin} sx={{ mt: 1 }}>
             <TextField
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               margin="normal"
               required
               fullWidth
+              type="email"
               id="email"
-              label="Email Address"
+              label="Email or Name"
               name="email"
-              autoComplete="email"
               autoFocus
             />
             <TextField
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               margin="normal"
               required
               fullWidth
@@ -83,7 +105,7 @@ function Signin() {
           </Box>
         </Box>
       </Grid>
-    </Grid>
+    </Grid >
   );
 }
 
--- app/mtrs_backend/backend.py
+++ app/mtrs_backend/backend.py
@@ -30,6 +30,31 @@ def login():
         return jsonify({'message': 'User Login successful'}), 200
     else:
         return jsonify({'message': 'Invalid credentials'}), 401
+    
+@app.route('/admin/login', methods=['POST'])
+def adminlogin():
+    data = request.json
+    admin_name = data.get('email')
+    password = data.get('password')
+    db = mysql.connector.connect(
+        host="localhost",
+        user="mtrs",
+        password="ds5110",
+        database="smartcine"
+    )
+
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute('SELECT * FROM admin WHERE admin_name = %s AND password = %s', (admin_name, password))
+
+    admin = cursor.fetchone()
+    cursor.close()
+    db.close()
+
+    if admin:
+        return jsonify({'message': 'Admin Login successful'}), 200
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 401
 
 if __name__ == '__main__':
     app.run(debug=True)
