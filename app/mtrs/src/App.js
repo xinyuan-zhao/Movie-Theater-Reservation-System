0df0743695e3638d5e04654a57131503f6ce6198
Shawn Fei <shawnfei2001@gmail.com>
2023-11-28 10:51:57 -0500

    set auto increment db, signup finished

--- app/mtrs/src/App.js
+++ app/mtrs/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
